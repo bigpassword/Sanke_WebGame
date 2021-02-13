d90b7453375cf722b279fd1f693226c18e507c99
fix3Dlemonade <44777318+fix3Dlemonade@users.noreply.github.com>
2021-02-13 23:36:42 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+﻿var boardSize = 7;
+var boardFill = 1;
+
+var board = [];
+
+for (let i = 0; i < boardSize; i++) {
+	var tempArray = [];
+	for (let e = 0; e < boardSize; e++) {
+		tempArray.push(boardFill);
+	}
+	board.push(tempArray);
+}
+
+function setup() {
+	alert('setup starting');
+	
+	alert('setup finished successfully');
+}
+
+function printBoard() {
+	document.getElementById('content').innerHTML = '';
+
+	for (let i = 0; i < board.length; i++) {
+		document.getElementById('content').innerHTML += board[i] + '<br>';
+	}
+}
+
+function input() {
+
+}
